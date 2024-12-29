db2abfa9368b1f6ca0d1c66b19aa5ddc83a5276b
idsyr <idsyr@mail.ru>
2024-12-30 03:44:21 +0500

    newer ver: bd.sql

--- inst_test/bd.sql
+++ inst_test/bd.sql
@@ -1,31 +1,31 @@
-CREATE DATABASE testing;
-USE testing;
+CREATE DATABASE cource_test;
+USE cource_test;
 CREATE TABLE object_type(
-     ID int,
+     ID int primary key,
      name varchar(20)
 );
 CREATE TABLE state(
-       ID int,
+       ID int primary key,
        name varchar(20)
 );
 CREATE TABLE result_type(
-       ID int,
+       ID int primary key,
        name varchar(20)
 );
 CREATE TABLE positions(
-       ID int,
+       ID int primary key,
        name varchar(15)
 );
 CREATE TABLE employees(
-       ID int,
+       ID int primary key,
        name varchar(30),
        shortname varchar(5),
        password varchar(20),
        positionsID int,
-       FOREIGN KEY (positionsID) REFERENCES positions(ID)
+       FOREIGN KEY (positionsID) REFERENCES positions (ID)
 );
 CREATE TABLE project(
-       ID int,
+       ID int primary key,
        name varchar(20),
        version varchar(10)
 );
@@ -33,12 +33,12 @@ CREATE TABLE object(
        object_typeID int,
        projectID int,
        employeesID int,
-       result_typeID,
+       result_typeID int,
        name varchar(30),
        comment varchar(40),       
-       FOREIGN KEY (employeesID) REFERENCES employees (ID)
-       FOREIGN KEY (employeesID) REFERENCES employees (ID)
-       FOREIGN KEY (projectID) REFERENCES project (ID)
+       FOREIGN KEY (employeesID) REFERENCES employees (ID),
+       FOREIGN KEY (employeesID) REFERENCES employees (ID),
+       FOREIGN KEY (projectID) REFERENCES project (ID),
        FOREIGN KEY (object_typeID) REFERENCES object_type (ID)
 );
 
